fca1b409afd7ff20254cb7b1d047250e5c50d85f
matiasrepo <matiasautomot@gmail.com>
2023-05-19 21:38:56 -0300

    upd

---
+++ Modulo 2/input.py
@@ -0,0 +1,12 @@
+####################################
+# ENTRADA DE DATOS - FUNCION input()
+####################################
+
+'''
+La función input() permite a los usuarios introducir datos de distintos tipos
+desde la entrada estándar (normalmente se corresponde con la entrada de un teclado).
+Y estos valores ingresados se pueden guardar en una variaable
+'''
+
+nombre = input('Escribe tu nombre: ')
+print(nombre)
