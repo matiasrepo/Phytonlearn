bc17f4b4d395e7485f0f9005ed3a5d286621ba8b
matiasrepo <matiasautomot@gmail.com>
2023-05-19 21:28:48 -0300

    upd

--- Modulo 2/listas.py
+++ Modulo 2/listas.py
@@ -65,6 +65,43 @@ print(numeros_primos[5])
 print(numeros_primos)
 
 
-numeros_primos.insert(2, 3.14)
+numeros_primos.insert(2, 3.14) # type: ignore
 print(numeros_primos[2])
 print(numeros_primos)
+
+
+
+# Eliminar elementos  y longitud de una Listas
+
+'''Para realizarlos usamos las sintaxis: 
+
+del nombre_lista[indice]
+
+len(nombre_lista)'''
+
+numeros_primos = [2, 3, 5, 7, 11]
+print (numeros_primos)
+del numeros_primos[2]
+
+print (numeros_primos)
+
+len(numeros_primos) #len nos dice la longitud o 
+# cantidad de elementos que tiene una lista
+#es abreviacion de lenght
+
+
+'''Por último, para crear una lista vacía  usamos dos corchetes sin elementos:
+Esto puede resultar útil cuando no  conocemos los elementos de la lista al  momento de crearla, 
+pero los iremos  añadiendo vía append o insert en el  transcurso del programa.
+'''
+lista_vacia = []
+print(lista_vacia)
+
+lista_vacia.append(17)
+
+print(lista_vacia)
+
+lista_vacia.append(300)
+
+print(lista_vacia)
+
