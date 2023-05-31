710be133cce0ca294e76c33f65070da0a768f361
matiasrepo <matiasautomot@gmail.com>
2023-05-31 20:52:36 -0300

    upd

--- Modulo 2/bucleFOR.py
+++ Modulo 2/bucleFOR.py
@@ -8,9 +8,62 @@ bloque de código, tantas veces como elementos contenga unalista.
 """
 
 alumnos = ["Matias", "Jorge", "Martin", "Pablo"]
-#for alumno in alumnos:
-#    print("Hola mundo")
+for alumno in alumnos:
+    print("Hola mundo")
 
-print ("_______________")
+print("_______________")
 for alumno in alumnos:
     print(alumno)
+
+print("___________________________________________")
+
+print("Comienzo")
+for i in [0, 1, 2]:
+    print("Hola ", end="")
+print()
+print("Final")
+
+print("___________________________________________")
+print("Comienzo")
+for i in []:
+    print("Hola ", end="")
+print()
+print("Final")
+
+print("_________________________________________")
+print("Comienzo")
+for i in [1, 1, 1]:
+    print("Hola ", end="")
+print()
+print("Final")
+
+print("_________________________________________")
+print("Comienzo")
+for _ in [0, 1, 2]:
+    print("Hola ", end="")
+print()
+print("Final")
+
+print("________________________________________")
+print("Comienzo")
+for i in [3, 4, 5]:
+    print(f"Hola. Ahora i vale {i} y su cuadrado {i ** 2}")
+
+print("Final")
+
+
+
+i = 10
+print(f"El bucle no ha comenzado. Ahora i vale {i}")
+
+for i in [0, 1, 2, 3, 4]:
+    print(f"{i} * {i} = {i ** 2}")
+
+print(f"El bucle ha terminado. Ahora i vale {i}")
+
+
+
+print("_____________________________________")
+for i in "AMIGO":
+    print(f"Dame una {i}")
+print("¡AMIGO!")
